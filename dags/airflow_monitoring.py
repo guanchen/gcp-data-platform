4857b27791e4ac9eddac4a8c2aac69e19b085ccf
guanchen <gsong.pro@gmail.com>
2022-05-30 12:04:52 +0200

    Format based on black linter

--- dags/airflow_monitoring.py
+++ dags/airflow_monitoring.py
@@ -5,22 +5,24 @@ from airflow.operators.bash_operator import BashOperator
 from datetime import timedelta
 
 default_args = {
-    'start_date': airflow.utils.dates.days_ago(0),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
+    "start_date": airflow.utils.dates.days_ago(0),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
 dag = DAG(
-    'airflow_monitoring',
+    "airflow_monitoring",
     default_args=default_args,
-    description='liveness monitoring dag',
+    description="liveness monitoring dag",
     schedule_interval=None,
-    dagrun_timeout=timedelta(minutes=20))
+    dagrun_timeout=timedelta(minutes=20),
+)
 
 # priority_weight has type int in Airflow DB, uses the maximum.
 t1 = BashOperator(
-    task_id='echo',
-    bash_command='echo test',
+    task_id="echo",
+    bash_command="echo test",
     dag=dag,
     depends_on_past=False,
-    priority_weight=2**31-1)
+    priority_weight=2**31 - 1,
+)
